0cfbdfd442ec9c1b2047cc6ca5297bec70cd21c4
Abrar Faseeh <165325821+abrar-faseeh01@users.noreply.github.com>
2026-06-22 20:30:21 +0600

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        unordered_map<char, int> freq;
+        int maxFreq = 0;
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < n; right++) {
+            // Add current character to the window
+            freq[s[right]]++;
+            // Update the highest frequency character in the window
+            maxFreq = max(maxFreq, freq[s[right]]);
+
+            int windowLen = right - left + 1;
+            // If window is invalid, shrink from the left
+            if (windowLen - maxFreq > k) {
+                freq[s[left]]--;
+                left++;
+            } else {
+                // Update the longest valid window
+                maxLen = max(maxLen, right - left + 1);
+            }
+        }
+        return maxLen;
+    }
+};
